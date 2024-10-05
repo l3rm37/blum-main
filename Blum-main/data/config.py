6e71566224ae65a67b8402473794f630cba25776
l3rm37 <lermovl@gmail.com>
2024-10-05 13:57:27 +0700

    Add files via upload

---
+++ Blum-main/data/config.py
@@ -0,0 +1,42 @@
+# api id, hash
+API_ID = 26392210
+API_HASH = 'e36c22dec17cd63246856063310f341d'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '283993:kdmioieiweikiokeocki4okew' # API TOKEN get in @BotFather
+CHAT_ID = '22803822' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 15]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = False # True/False
+
+# скок поинтов с игры
+POINTS = [100, 140] #[min, max]
+
+# тратить алмазы
+SPEND_DIAMONDS = True # True/False
+
+# делать таски
+DO_TASKS = True
+
+# сон между играми
+SLEEP_GAME_TIME = [30,50] #[min,max]
+
+# мини задержки
+MINI_SLEEP = [3,7] #[min,max]
+
+# доп задержка после 8часов 
+SLEEP_8HOURS = [60*60,120*60] #[min,max] seconds
+
+hello ='''
+made by l3rm37 & petya
+
+'''
